0667a7ada7081819f308f52ba588742bf1c68780
stephenhartman <stephen.c.hartman@gmail.com>
2017-06-07 14:27:02 -0400

    Added form update

--- spec/features/edit_movie_spec.rb
+++ spec/features/edit_movie_spec.rb
@@ -10,6 +10,13 @@ describe "Editing a movie" do
     click_link 'Edit'
 
     expect(current_path).to eq(edit_movie_path(movie))
-    expect(find_field('Title')).to eq(movie.title)
+    expect(find_field('Title').value).to eq(movie.title)
+
+    fill_in 'Title', with: "Updated Movie Title"
+
+    click_button 'Update Movie'
+
+    expect(current_path).to eq(movie_path(movie))
+    expect(page).to have_text('Updated Movie Title')
   end
 end
