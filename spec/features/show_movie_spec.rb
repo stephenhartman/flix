990a55ee70732d041d4b07197589bdc06d8a83b5
stephenhartman <stephen.c.hartman@gmail.com>
2017-06-07 12:53:40 -0400

    Got past TDD tutorial

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -4,19 +4,30 @@ describe "Viewing the individual movie pages" do
 
   it "shows the movie" do
 
-    movie = Movie.create(title: "Iron Man",
-                          rating: "PG-13",
-                          total_gross: 318412101.00,
-                          description: "Tony Stark builds an armored suit to fight the throes of evil",
-                          released_on: "2008-05-02")
-    visit movie_url(movie)
+    movie = Movie.create(movie_attributes(total_gross: 318412101.00))
+    
+    visit "http://example.com/movies/#{movie.id}"
 
     expect(page).to have_text(movie.title)
     expect(page).to have_text(movie.rating)
-    expect(page).to have_text("$318,412,101.00")
     expect(page).to have_text(movie.description[0..10])
     expect(page).to have_text(movie.released_on)
+  end
 
+  it "shows the total gross if total gross exceeds $50 million" do
+    movie = Movie.create(movie_attributes(total_gross: 60000000.00))
+
+    visit movie_url(movie)
+
+    expect(page).to have_text("$60,000,000.00")
+  end
+
+  it "shows 'Flop!' if the total gross is less than $50 million" do
+    movie = Movie.create(movie_attributes(total_gross: 0))
+
+    visit movie_url(movie)
+
+    expect(page).to have_text("Flop!")
   end
 
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Movie, type: :model do
+  it "is a flop if the total gross is less than $50 million" do
+    movie = Movie.new(total_gross: 40000000.00)
+
+    expect(movie.flop?).to be_truthy
+  end
+
+  it "shows the total gross if greater than $50 million" do
+    movie = Movie.new(total_gross: 50000000.00)
+
+    expect(movie.flop?).to be_falsey
+  end
 end
