d1755530b3a6a0dbd2c13afde728354373b1ad99
stephenhartman <stephen.c.hartman@gmail.com>
2017-06-29 13:39:11 -0400

    Major stuff, reviews

--- spec/features/edit_movie_spec.rb
+++ spec/features/edit_movie_spec.rb
@@ -20,6 +20,14 @@ describe "Editing a movie" do
     expect(current_path).to eq(movie_path(movie))
 
     expect(page).to have_text('Updated Movie Title')
+    expect(page).to have_text('Movie successfully updated!')
   end
 
+  it "does not update a movie with invalid data" do
+    movie = Movie.create(movie_attributes)
+    visit edit_movie_url(movie)
+    fill_in 'Title', with: " "
+    click_button 'Update Movie'
+    expect(page).to have_text('error')
+  end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,6 +1,95 @@
 require 'rails_helper'
 
 describe "A movie" do
+  it "requires a title" do
+    movie = Movie.new(title: "")
+    movie.valid?
+
+    expect(movie.errors[:title].any?).to eq(true)
+  end
+
+  it "requires a description" do
+    movie = Movie.new(description: "")
+    movie.valid?
+
+    expect(movie.errors[:description].any?).to eq(true)
+  end
+
+  it "requires a released on date" do
+    movie = Movie.new(released_on: "")
+    movie.valid?
+
+    expect(movie.errors[:released_on].any?).to eq(true)
+  end
+
+  it "requires a duration" do
+    movie = Movie.new(duration: "")
+    movie.valid?
+
+    expect(movie.errors[:duration].any?).to eq(true)
+  end
+
+  it "requires a description over 24 characters" do
+    movie = Movie.new(description: "x" * 24)
+    movie.valid?
+
+    expect(movie.errors[:description].any?).to eq(true)
+  end
+
+  it "accepts a $0 total gross" do
+    movie = Movie.new(total_gross: 0)
+    movie.valid?
+
+    expect(movie.errors[:total_gross].any?).to eq(false)
+  end
+
+  it "rejects a negative total gross" do
+    movie = Movie.new(total_gross: -100.00)
+    movie.valid?
+
+    expect(movie.errors[:total_gross].any?).to eq(true)
+  end
+
+  it "accepts properly formatted image file names" do
+    file_names = %w[e.png movie.png movie.jpg movie.gif MOVIE.GIF]
+    file_names.each do |file_name|
+      movie = Movie.new(image_file_name: file_name)
+      movie.valid?
+      expect(movie.errors[:image_file_name].any?).to eq(false)
+    end
+  end
+
+  it "rejects improperly formatted image file names" do
+    file_names = %w[.png .jpg .gif movie.pdf movie.doc]
+    file_names.each do |file_name|
+      movie = Movie.new(image_file_name: file_name)
+      movie.valid?
+      expect(movie.errors[:image_file_name].any?).to eq(true)
+    end
+  end
+
+  it "accepts any rating that is in the approved list" do
+    ratings = %w(G PG PG-13 R NC-17)
+    ratings.each do |rating|
+      movie = Movie.new(rating: rating)
+      movie.valid?
+      expect(movie.errors[:rating].any?).to eq(false)
+    end
+  end
+
+  it "rejects any rating that is not on the approved list" do
+    ratings = %w(G-1 P G-13 R-9 C-17)
+    ratings.each do |rating|
+      movie = Movie.new(rating: rating)
+      movie.valid?
+      expect(movie.errors[:rating].any?).to eq(true)
+    end
+  end
+
+  it "is valid with example attributes" do
+    movie = Movie.new(movie_attributes)
+    expect(movie.valid?).to eq(true)
+  end
   it "is a flop if the total gross is less than $50M" do
     movie = Movie.new(total_gross: 40000000.00)
 
@@ -11,5 +100,21 @@ describe "A movie" do
     movie = Movie.new(total_gross: 60000000.00)
 
     expect(movie.flop?).to eq(false)
+  end
+
+  it "has many reviews" do
+    movie = Movie.new(movie_attributes)
+    review1 = movie.reviews.new(review_attributes)
+    review2 = movie.reviews.new(review_attributes)
+
+    expect(movie.reviews).to include(review1)
+    expect(movie.reviews).to include(review2)
+  end
+
+  it "deletes associated reviews" do
+    movie = Movie.create(movie_attributes)
+    movie.reviews.create(review_attributes)
+
+    expect { movie.destroy }.to change(Review, :count).by(-1)
   end
 end
